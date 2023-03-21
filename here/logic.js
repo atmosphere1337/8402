ba3f627bff8680b5cd87ba94c191cf81c3540177
atmosphere1337 <agbdlcider@gmail.com>
2023-03-21 12:00:03 +0800

    Update logic.js

--- here/logic.js
+++ here/logic.js
@@ -1,4 +1,8 @@
-let map = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+let map = [[0,0,0,0],
+           [0,8,4,0],
+           [2,2,4,4],
+           [2,0,2,4]];
+let current_score = 0;
 function line( x )
 {
     x = x.filter(x=>x!=0);
@@ -16,9 +20,41 @@ function line( x )
         out = out.concat(new Array(4-out.length).fill(0));
     return out;
 }
-function square()
-{
-    
+function square(rotation)
+{   
+    switch (rotation)
+    {
+        case 1://  left
+            for (i in map)
+                map[i] = line(map[i]);
+            break;
+        case 2: // up
+            for (i = 0; i < 4; i++)
+            {
+                buf = line([map[0][i], map[1][i], map[2][i], map[3][i]]);
+                map[0][i] = buf[0];
+                map[1][i] = buf[1];
+                map[2][i] = buf[2];
+                map[3][i] = buf[3];                
+            }
+            break;
+        case 3: // right
+            for (i in map)
+                map[i] = line(map[i].reverse()).reverse(); // 
+            break;
+        case 4: // down
+        for (i = 0; i < 4; i++)
+        {
+            buf = line([map[3][i], map[2][i], map[1][i], map[0][i]]);
+            map[0][i] = buf[3];
+            map[1][i] = buf[2];
+            map[2][i] = buf[1];
+            map[3][i] = buf[0];                
+        }
+            break;
+        default:
+            break;
+    }
 }
-
-console.log(line([2,2,4,4]));
+square(4);
+console.table(map);
